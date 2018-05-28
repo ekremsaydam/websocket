5dd444cc8633eb391dd197ab0fca10040eaf8f06
ekremsaydam <30255407+ekremsaydam@users.noreply.github.com>
2018-05-28 22:38:01 +0300

    socket.io emit

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const http = require('http');
-
 const socketio = require('socket.io');
-
 
 const server = http.createServer((req, res) => {
   res.end('hey');
@@ -13,4 +11,17 @@ const io = socketio.listen(server);
 
 io.sockets.on('connection', (socket) => {
   console.log('Kullanıcı bağlandı');
+
+  // setInterval(() => {
+  //   socket.emit('merhaba de', { location: 'Türkiye' });
+  // }, 1000);
+
+  socket.on('selamVer', (data) => {
+    console.log(data.city)
+    console.log(data.name)
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Kullanıcı ayrıldı');
+  });
 });
